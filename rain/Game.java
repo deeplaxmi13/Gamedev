132c6092e5bec9996d4eee091dc389d705b0cc62
deeplaxmi13 <35959484+deeplaxmi13@users.noreply.github.com>
2018-02-01 18:37:33 +0530

    Update Game.java

--- rain/Game.java
+++ rain/Game.java
@@ -16,6 +16,7 @@ public class Game extends Canvas implements Runnable {
 	public static int width = 300;
 	public static int height = width / 16 * 9;
 	public static int scale = 3;
+	public static String title = "Rain";
 
 	private Thread thread;
 	private JFrame frame;
@@ -50,10 +51,34 @@ public class Game extends Canvas implements Runnable {
 	}
 
 	public void run() {
+		long lastTime = System.nanoTime();
+		long timer = System.currentTimeMillis();
+		final double ns = 1000000000.0 / 60.0;
+		double delta = 0;
+		int frames = 0;
+		int updates = 0;
 		while (running) {
-			update();
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while (delta >= 1) {
+				update();
+				updates++;
+				delta--;
+			}
 			render();
+			frames++;
+
+			if (System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+				System.out.println(updates + " ups, " + frames + " fps");
+				frame.setTitle(title + ": " + updates + " ups, " + frames + " fps"); 
+				updates = 0;
+				frames = 0;
+			}
+
 		}
+		stop();
 	}
 
 	private void render() {
@@ -81,12 +106,12 @@ public class Game extends Canvas implements Runnable {
 	public static void main(String args[]) {
 		Game game = new Game();
 		game.frame.setResizable(false);
+		game.frame.setTitle(Game.title);
 		game.frame.add(game);
 		game.frame.pack();
 		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		game.frame.setLocationRelativeTo(null);
 		game.frame.setVisible(true);
-
 		game.start();
 	}
 }
