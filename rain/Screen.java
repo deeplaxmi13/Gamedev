6ff2eca551224354cb2f32d4274b2ff35cfda44a
deeplaxmi13 <35959484+deeplaxmi13@users.noreply.github.com>
2018-02-01 18:39:14 +0530

    Update Screen.java

--- rain/Screen.java
+++ rain/Screen.java
@@ -9,7 +9,43 @@ public class Screen {
 	public Screen(int width, int height) {
 		this.width = width;
 		this.height = height;
+	package rain;
+
+public class Screen {
+	private int width, height;
+	public int[] pixels;
+	int xtime = width - 1, ytime = height - 1;
+	int counter = 0;
+
+	public Screen(int width, int height) {
+		this.width = width;
+		this.height = height;
 		pixels = new int[width * height];
+	}
+
+	public void clear() {
+		for (int i = 0; i < pixels.length; i++) {
+			pixels[i] = 0;
+		}
+	}
+
+	public void render() {
+		counter++;
+		if (counter % 50 == 0)
+			xtime--;
+		if (counter % 50 == 0)
+			ytime--;
+		for (int y = 0; y < height; y++) {
+			if (ytime >= height || ytime < 0)
+				break;
+			for (int x = 0; x < width; x++) {
+				if (xtime >= width || xtime < 0)
+					break;
+				pixels[xtime + ytime * width] = 0xff00ff;
+			}
+		}
+	}
+}	pixels = new int[width * height];
 	}
 
 	public void clear() {
